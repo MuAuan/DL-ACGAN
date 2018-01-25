b80a8f43015676c5a3ae48a1420e2d803770ab8e
MuAuan <35788955+MuAuan@users.noreply.github.com>
2018-01-25 21:07:49 +0900

    Add files via upload

---
+++ DVGG16QG1024L4Mini4_model.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Train an Auxiliary Classifier Generative Adversarial Network (ACGAN) on the
+MNIST dataset. See https://arxiv.org/abs/1610.09585 for more details.
+
+You should start to see reasonable images after ~5 epochs, and good images
+by ~15 epochs. You should use a GPU, as the convolution-heavy operations are
+very slow on the CPU. Prefer the TensorFlow backend if you plan on iterating,
+as the compilation time can be a blocker using Theano.
+
+Timings:
+
+Hardware           | Backend | Time / Epoch
+--60000Data-----------------------------------------
+ CPU               | TF      | 3 hrs
+ Titan X (maxwell) | TF      | 4 min
+ Titan X (maxwell) | TH      | 7 min
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+conv2d_1 (Conv2D)            (None, 8, 32, 32)         224
+_________________________________________________________________
+activation_1 (Activation)    (None, 8, 32, 32)         0
+_________________________________________________________________
+dropout_1 (Dropout)          (None, 8, 32, 32)         0
+_________________________________________________________________
+conv2d_2 (Conv2D)            (None, 8, 32, 32)         584
+_________________________________________________________________
+activation_2 (Activation)    (None, 8, 32, 32)         0
+_________________________________________________________________
+max_pooling2d_1 (MaxPooling2 (None, 8, 16, 16)         0
+_________________________________________________________________
+dropout_2 (Dropout)          (None, 8, 16, 16)         0
+_________________________________________________________________
+conv2d_3 (Conv2D)            (None, 32, 16, 16)        2336
+_________________________________________________________________
+activation_3 (Activation)    (None, 32, 16, 16)        0
+_________________________________________________________________
+dropout_3 (Dropout)          (None, 32, 16, 16)        0
+_________________________________________________________________
+conv2d_4 (Conv2D)            (None, 32, 16, 16)        9248
+_________________________________________________________________
+activation_4 (Activation)    (None, 32, 16, 16)        0
+_________________________________________________________________
+max_pooling2d_2 (MaxPooling2 (None, 32, 8, 8)          0
+_________________________________________________________________
+dropout_4 (Dropout)          (None, 32, 8, 8)          0
+_________________________________________________________________
+conv2d_5 (Conv2D)            (None, 1024, 8, 8)        295936
+_________________________________________________________________
+activation_5 (Activation)    (None, 1024, 8, 8)        0
+_________________________________________________________________
+dropout_5 (Dropout)          (None, 1024, 8, 8)        0
+_________________________________________________________________
+conv2d_6 (Conv2D)            (None, 1024, 8, 8)        9438208
+_________________________________________________________________
+activation_6 (Activation)    (None, 1024, 8, 8)        0
+_________________________________________________________________
+dropout_6 (Dropout)          (None, 1024, 8, 8)        0
+_________________________________________________________________
+conv2d_7 (Conv2D)            (None, 1024, 8, 8)        9438208
+_________________________________________________________________
+activation_7 (Activation)    (None, 1024, 8, 8)        0
+_________________________________________________________________
+max_pooling2d_3 (MaxPooling2 (None, 1024, 4, 4)        0
+_________________________________________________________________
+dropout_7 (Dropout)          (None, 1024, 4, 4)        0
+_________________________________________________________________
+flatten_1 (Flatten)          (None, 16384)             0
+=================================================================
+Total params: 19,184,744
+Trainable params: 19,184,744
+Non-trainable params: 0
+_________________________________________________________________
+__________________________________________________________________________________________________
+Layer (type)                    Output Shape         Param #     Connected to                   
+==================================================================================================
+input_1 (InputLayer)            (None, 3, 32, 32)    0                                          
+__________________________________________________________________________________________________
+sequential_1 (Sequential)       (None, 16384)        19184744    input_1[0][0]                  
+__________________________________________________________________________________________________
+generation (Dense)              (None, 1)            16385       sequential_1[1][0]             
+__________________________________________________________________________________________________
+auxiliary (Dense)               (None, 10)           163850      sequential_1[1][0]             
+==================================================================================================
+Total params: 19,364,979
+Trainable params: 19,364,979
+Non-trainable params: 0
+__________________________________________________________________________________________________
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+dense_1 (Dense)              (None, 2048)              206848
+_________________________________________________________________
+dense_2 (Dense)              (None, 2048)              4196352
+_________________________________________________________________
+reshape_1 (Reshape)          (None, 8, 16, 16)         0
+_________________________________________________________________
+up_sampling2d_1 (UpSampling2 (None, 8, 32, 32)         0
+_________________________________________________________________
+conv2d_8 (Conv2D)            (None, 32, 32, 32)        2336
+_________________________________________________________________
+conv2d_9 (Conv2D)            (None, 3, 32, 32)         387
+=================================================================
+Total params: 4,405,923
+Trainable params: 4,405,923
+Non-trainable params: 0
+_________________________________________________________________
+__________________________________________________________________________________________________
+Layer (type)                    Output Shape         Param #     Connected to                   
+==================================================================================================
+input_3 (InputLayer)            (None, 1)            0                                          
+__________________________________________________________________________________________________
+embedding_1 (Embedding)         (None, 1, 100)       1000        input_3[0][0]                  
+__________________________________________________________________________________________________
+input_2 (InputLayer)            (None, 100)          0                                          
+__________________________________________________________________________________________________
+flatten_2 (Flatten)             (None, 100)          0           embedding_1[0][0]              
+__________________________________________________________________________________________________
+multiply_1 (Multiply)           (None, 100)          0           input_2[0][0]                  
+                                                                 flatten_2[0][0]                
+__________________________________________________________________________________________________
+sequential_2 (Sequential)       (None, 3, 32, 32)    4405923     multiply_1[0][0]               
+==================================================================================================
+Total params: 4,406,923
+Trainable params: 4,406,923
+Non-trainable params: 0
+__________________________________________________________________________________________________
+X_train.shape1 (50000, 3, 32, 32)
+X_train.shape2 (50000, 3, 32, 32)
+component              | loss | generation_loss | auxiliary_loss
+-----------------------------------------------------------------
+generator (train)      | 1.75 | 1.67            | 0.09
+generator (test)       | 1.39 | 1.32            | 0.06
+discriminator (train)  | 0.72 | 0.45            | 0.27
+discriminator (test)   | 1.00 | 0.45            | 0.56
+generated_images (100, 32, 32, 3)
+
+"""
+from __future__ import print_function
+
+from collections import defaultdict
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+from PIL import Image
+
+from six.moves import range
+
+import keras.backend as K
+from keras.datasets import mnist
+from keras import layers
+from keras.layers import Input, Dense, Reshape, Flatten, Embedding, Dropout, Activation
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+from keras.utils.generic_utils import Progbar
+import numpy as np
+from glob import glob
+from keras.layers import MaxPooling2D
+
+np.random.seed(1337)
+
+K.set_image_data_format('channels_first')
+
+
+
+def build_generator(latent_size):
+    # we will map a pair of (z, L), where z is a latent vector and L is a
+    # label drawn from P_c, to image space (..., 1, 32, 32)
+    cnn = Sequential()
+
+    cnn.add(Dense(256, input_dim=latent_size, activation='relu'))
+    cnn.add(Dense(3 * 32 * 32, activation='relu'))
+    cnn.add(Reshape((3, 32, 32)))
+    
+    # upsample to (..., 16, 16)
+    cnn.add(UpSampling2D(size=(2, 2)))
+    cnn.add(Conv2D(128, (3, 3), padding='same',
+                   activation='relu',
+                   kernel_initializer='glorot_normal'))
+    
+     # upsample to (..., 32, 32)
+    cnn.add(UpSampling2D(size=(2, 2)))
+    cnn.add(Conv2D(32, (3, 3), padding='same',
+                   activation='relu',
+                   kernel_initializer='glorot_normal'))
+
+
+
+    # take a channel axis reduction
+    cnn.add(Conv2D(3, (2, 2), padding='same',
+                   activation='tanh',
+                   kernel_initializer='glorot_normal'))
+    
+    cnn.summary()
+
+    # this is the z space commonly refered to in GAN papers
+    latent = Input(shape=(latent_size, ))
+
+    # this will be our label
+    image_class = Input(shape=(1, ), dtype='int32')
+
+    # 10 classes in MNIST
+    num_classes=10
+    cls = Flatten()(Embedding(num_classes, latent_size,
+                              embeddings_initializer='glorot_normal')(image_class)) #10
+
+    # hadamard product between z-space and a class conditional embedding
+    h = layers.multiply([latent, cls])
+
+    fake_image = cnn(h)
+
+    return Model([latent, image_class], fake_image)
+
+
+def build_discriminator():
+    # build a relatively standard conv net, with LeakyReLUs as suggested in
+    # the reference paper
+    cnn = Sequential()
+    cnn.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=(3, 128, 128)))
+    cnn.add(Activation('relu'))
+    cnn.add(Dropout(0.25))
+    cnn.add(Conv2D(32, (3, 3), padding='same'))
+    cnn.add(Activation('relu'))
+    cnn.add(MaxPooling2D(pool_size=(2, 2)))
+    cnn.add(Dropout(0.25))
+    
+    cnn.add(Conv2D(64, (3, 3), padding='same'))
+    cnn.add(Activation('relu'))
+    cnn.add(Dropout(0.25))
+    cnn.add(Conv2D(64, (3, 3), padding='same'))
+    cnn.add(Activation('relu'))
+    cnn.add(MaxPooling2D(pool_size=(2, 2)))
+    cnn.add(Dropout(0.25))
+    
+    cnn.add(Conv2D(128,(3, 3), padding='same'))
+    cnn.add(Activation('relu'))
+    cnn.add(Dropout(0.25))
+    cnn.add(Conv2D(128, (3, 3), padding='same'))
+    cnn.add(Activation('relu'))
+    cnn.add(Dropout(0.25))
+    cnn.add(Conv2D(128, (3, 3), padding='same'))
+    cnn.add(Activation('relu'))
+    cnn.add(MaxPooling2D(pool_size=(2, 2)))
+    cnn.add(Dropout(0.25))
+    
+    cnn.add(Conv2D(256, (3, 3), padding='same'))
+    cnn.add(Activation('relu'))
+    cnn.add(Dropout(0.25))
+    cnn.add(Conv2D(256, (3, 3), padding='same'))
+    cnn.add(Activation('relu'))
+    cnn.add(Dropout(0.25))
+    cnn.add(Conv2D(256, (3, 3), padding='same'))
+    cnn.add(Activation('relu'))
+    cnn.add(MaxPooling2D(pool_size=(2, 2)))
+    cnn.add(Dropout(0.25))
+
+    cnn.add(Flatten())
+    cnn.summary()
+    
+    image = Input(shape=(3, 128, 128))
+
+    features = cnn(image)
+
+    # first output (name=generation) is whether or not the discriminator
+    # thinks the image that is being shown is fake, and the second output
+    # (name=auxiliary) is the class that the discriminator thinks the image
+    # belongs to.
+    num_classes=10
+    fake = Dense(1, activation='sigmoid', name='generation')(features)
+    aux = Dense(num_classes, activation='softmax', name='auxiliary')(features) #num_classes=10
+
+    return Model(image, [fake, aux])
---
+++ acgan_vgg16QMini_color_Fitting4.py
@@ -0,0 +1,365 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Train an Auxiliary Classifier Generative Adversarial Network (ACGAN) on the
+MNIST dataset. See https://arxiv.org/abs/1610.09585 for more details.
+
+You should start to see reasonable images after ~5 epochs, and good images
+by ~15 epochs. You should use a GPU, as the convolution-heavy operations are
+very slow on the CPU. Prefer the TensorFlow backend if you plan on iterating,
+as the compilation time can be a blocker using Theano.
+
+Timings:
+
+Hardware           | Backend | Time / Epoch
+-------------------------------------------
+ CPU               | TF      | 3 hrs
+ Titan X (maxwell) | TF      | 4 min
+ Titan X (maxwell) | TH      | 7 min
+
+Consult https://github.com/lukedeo/keras-acgan for more information and
+example output
+"""
+from __future__ import print_function
+
+from collections import defaultdict
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+from PIL import Image
+
+from six.moves import range
+
+import keras.backend as K
+from keras.datasets import mnist
+from keras import layers
+from keras.layers import Input, Dense, Reshape, Flatten, Embedding, Dropout
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam,Adamax
+from keras.utils.generic_utils import Progbar
+import numpy as np
+from keras.datasets import cifar10, cifar100  #10
+import keras
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from DVGG16QG1024L4Mini4_model import build_generator, build_discriminator
+#from DQGQ_model import build_generator, build_discriminator
+
+from keras.preprocessing import image
+import sys
+import cv2
+import os
+
+
+np.random.seed(1337)
+
+K.set_image_data_format('channels_first')
+
+#その１　------データセット作成------
+
+#フォルダは整数で名前が付いています。
+def getDataSet():
+    #リストの作成
+    X_train = []
+    X_test = []
+    y_train = []
+    y_test = []
+
+    for i in range(0,9):
+        path = "./train_images"
+        if i == 0:
+            #othersは15000枚用意します。テスト用には3000枚
+            cutNum = 480
+            cutNum2 = 440
+        else:
+            #主要キャラたちは1800枚ずつ。テスト用には300枚
+            cutNum = 480
+            cutNum2 = 440
+        imgList = os.listdir(path+str(i))
+        print(imgList)
+        imgNum = len(imgList)
+        for j in range(cutNum):
+            #imgSrc = cv2.imread(path+str(i)+"/"+imgList[j])
+            img = image.load_img(path+str(i)+"/"+imgList[j], target_size=(128,128))
+            imgSrc = image.img_to_array(img)
+            #imreadはゴミを吸い込んでも、エラーで止まらずNoneを返してくれます。
+            #ですので読み込み結果がNoneでしたらスキップしてもらいます。
+            if imgSrc is None:continue
+            if j < cutNum2:
+                X_train.append(imgSrc)
+                y_train.append(i)
+            else:
+                X_test.append(imgSrc)
+                y_test.append(i)
+    print(len(X_train),len(y_train),len(X_test),len(y_test))
+
+    return X_train,y_train,X_test,y_test
+
+
+
+if __name__ == '__main__':
+
+    # batch and latent size taken from the paper
+    epochs = 201
+    batch_size = 32 #32
+    latent_size = 100
+    num_classes = 10 #10
+    
+    # Adam parameters suggested in https://arxiv.org/abs/1511.06434
+    #generator parames of Adamax
+    adam_lr1 = 0.0000125 #0.0001
+    adam_beta_11 = 0.5
+    
+    #discriminator params of Adam
+    adam_lr = 0.00002 #0.0002
+    adam_beta_1 = 0.5
+    
+    #keras default Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.00001)
+    #keras default Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    #keras default RMSprop(lr=0.001, rho=0.9, epsilon=1e-06)
+    
+    # build the discriminator
+    discriminator = build_discriminator()
+    discriminator.load_weights('params_discriminator_cifar10_epoch_200.hdf5')
+    #discriminator.trainable = False
+    discriminator.compile(
+        optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1, beta_2=0.999, epsilon=1e-08, decay=0.00001),
+        loss=['binary_crossentropy', 'sparse_categorical_crossentropy']
+    )
+    discriminator.summary()
+    
+    # build the generator
+    generator = build_generator(latent_size)
+    generator.load_weights('params_generator_cifar10_epoch_200.hdf5')
+    #generator.trainable = False
+    generator.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1, beta_2=0.999, epsilon=1e-08, decay=0.),
+                      loss='binary_crossentropy')
+    generator.summary()
+    
+    latent = Input(shape=(latent_size, ))
+    image_class = Input(shape=(1,), dtype='int32')
+
+    # get a fake image
+    fake = generator([latent, image_class])
+
+    # we only want to be able to train generation for the combined model
+    discriminator.trainable = False
+    fake, aux = discriminator(fake)
+    combined = Model([latent, image_class], [fake, aux])
+
+    combined.compile(
+        optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1),
+        loss=['binary_crossentropy', 'sparse_categorical_crossentropy']
+    )
+
+    # get our mnist data, and force it to be of shape (..., 1, 28, 28) with
+    # range [-1, 1]
+    #(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    #(x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine') #10
+    
+    
+    #上で作った関数でデータセットを用意します。
+    x_train,y_train,x_test,y_test = getDataSet()
+    #print(x_train)
+    #print(np.array(x_train))
+    
+    #このままだとchainerで読み込んでもらえないので、array型にします。
+    #x_train = np.array(x_train).astype(np.float32).reshape((len(x_train),3, 32, 32)) / 255
+    x_train = np.array(x_train)  / 255
+    y_train = np.array(y_train).astype(np.int32)
+    x_test = np.array(x_test) / 255
+    y_test = np.array(y_test).astype(np.int32)
+    
+   
+    X_train=x_train[0:3960]  #.transpose(0,2,3,1) 1760
+    print("X_train.shape1",X_train.shape)
+    X_test=x_test[0:360]   #.transpose(0,2,3,1) 160
+    y_train=y_train[0:3960].reshape(1,3960)[0] #1750
+    y_test=y_test[0:360].reshape(1,360)[0]
+    
+    for i in range(13):
+        plt.figure(num=None, figsize=(10, 10), dpi=50)
+        plt.imshow(X_train.transpose(0,2,3,1)[i][0:127][0:127])
+        print(y_train[i])
+        plt.pause(1)
+        plt.close()
+        #plt.imshow(X_train.transpose(0,2,3,1)[i][0:31][0:31])        
+        #plt.pause(1)
+        #plt.close()
+    print(X_train)
+    #X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+    X_train = (X_train.astype(np.float32) - 0.5) / 0.5
+    print("X_train.shape2",X_train.shape)
+
+    #X_test = (X_test.astype(np.float32) - 127.5) / 127.5
+    X_test = (X_test.astype(np.float32) - 0.5) / 0.5
+    
+
+    num_train, num_test = X_train.shape[0], X_test.shape[0]
+
+    train_history = defaultdict(list)
+    test_history = defaultdict(list)
+
+    for epoch in range(epochs):
+        print('Epoch {} of {}'.format(epoch + 1, epochs))
+        if epoch%1:
+            discriminator.trainable = True
+            discriminator.compile(
+                optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1, beta_2=0.999, epsilon=1e-08, decay=0.),
+                loss=['binary_crossentropy', 'sparse_categorical_crossentropy']
+                )
+        else:
+            discriminator.trainable = True
+            discriminator.compile(
+                optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1, beta_2=0.999, epsilon=1e-08, decay=0.),
+                loss=['binary_crossentropy', 'sparse_categorical_crossentropy']
+                )
+
+        
+        num_batches = int(X_train.shape[0] / batch_size)
+        progress_bar = Progbar(target=num_batches)
+
+        epoch_gen_loss = []
+        epoch_disc_loss = []
+
+        for index in range(num_batches):
+            progress_bar.update(index)
+            # generate a new batch of noise
+            noise = np.random.uniform(-1, 1, (batch_size,latent_size))
+            # get a batch of real images
+            image_batch = X_train[index * batch_size:(index + 1) * batch_size]
+            label_batch = y_train[index * batch_size:(index + 1) * batch_size]
+            # sample some labels from p_c
+            sampled_labels = np.random.randint(0, num_classes, batch_size) #num_classes=10
+
+            # generate a batch of fake images, using the generated labels as a
+            # conditioner. We reshape the sampled labels to be
+            # (batch_size, 1) so that we can feed them into the embedding
+            # layer as a length one sequence
+            generated_images = generator.predict(
+                [noise, sampled_labels.reshape((-1, 1))], verbose=0)
+
+            X = np.concatenate((image_batch, generated_images))
+            y = np.array([1] * batch_size + [0] * batch_size)
+            aux_y = np.concatenate((label_batch, sampled_labels), axis=0)
+
+            # see if the discriminator can figure itself out...
+            epoch_disc_loss.append(discriminator.train_on_batch(X, [y, aux_y]))
+
+            # make new noise. we generate 2 * batch size here such that we have
+            # the generator optimize over an identical number of images as the
+            # discriminator
+            noise = np.random.uniform(-1, 1, (2 * batch_size, latent_size))
+            sampled_labels = np.random.randint(0, num_classes, 2 * batch_size)  #num_classes=10
+
+            # we want to train the generator to trick the discriminator
+            # For the generator, we want all the {fake, not-fake} labels to say
+            # not-fake
+            trick = np.ones(2 * batch_size)
+
+            epoch_gen_loss.append(combined.train_on_batch(
+                [noise, sampled_labels.reshape((-1, 1))],
+                [trick, sampled_labels]))
+
+        print('\nTesting for epoch {}:'.format(epoch + 1))
+
+        # evaluate the testing loss here
+
+        # generate a new batch of noise
+        noise = np.random.uniform(-1, 1, (num_test, latent_size))
+
+        # sample some labels from p_c and generate images from them
+        sampled_labels = np.random.randint(0, num_classes, num_test) #num_classes=10
+        generated_images = generator.predict(
+            [noise, sampled_labels.reshape((-1, 1))], verbose=False)
+
+        X = np.concatenate((X_test, generated_images))
+        y = np.array([1] * num_test + [0] * num_test)
+        
+        
+        aux_y = np.concatenate((y_test, sampled_labels), axis=0)
+
+        # see if the discriminator can figure itself out...
+        discriminator_test_loss = discriminator.evaluate(
+            X, [y, aux_y], verbose=False)
+        
+        discriminator_train_loss = np.mean(np.array(epoch_disc_loss), axis=0)
+        # make new noise
+        noise = np.random.uniform(-1, 1, (2 * num_test, latent_size))
+        sampled_labels = np.random.randint(0, num_classes, 2 * num_test)  #num_classes=10
+
+        trick = np.ones(2 * num_test)
+
+        generator_test_loss = combined.evaluate(
+            [noise, sampled_labels.reshape((-1, 1))],
+            [trick, sampled_labels], verbose=False)
+        
+        generator_train_loss = np.mean(np.array(epoch_gen_loss), axis=0)
+        
+        
+        # generate an epoch report on performance
+        train_history['generator'].append(generator_train_loss)
+        train_history['discriminator'].append(discriminator_train_loss)
+
+        test_history['generator'].append(generator_test_loss)
+        test_history['discriminator'].append(discriminator_test_loss)
+
+        print('{0:<22s} | {1:4s} | {2:15s} | {3:5s}'.format(
+            'component', *discriminator.metrics_names))
+        print('-' * 65)
+
+        ROW_FMT = '{0:<22s} | {1:<4.2f} | {2:<15.2f} | {3:<5.2f}'
+        print(ROW_FMT.format('generator (train)',
+                             *train_history['generator'][-1]))
+        print(ROW_FMT.format('generator (test)',
+                             *test_history['generator'][-1]))
+        print(ROW_FMT.format('discriminator (train)',
+                             *train_history['discriminator'][-1]))
+        print(ROW_FMT.format('discriminator (test)',
+                             *test_history['discriminator'][-1]))
+
+        if epoch%10==0:
+            # save weights every epoch
+            generator.save_weights(
+                'params_generator_cifar10_epoch_{0:03d}.hdf5'.format(epoch), True)
+            discriminator.save_weights(
+                'params_discriminator_cifar10_epoch_{0:03d}.hdf5'.format(epoch), True)
+
+            fig_sc=10
+            # generate some digits to display
+            noise = np.random.uniform(-1, 1, (fig_sc*fig_sc, latent_size))
+
+            sampled_labels = np.array([
+                [i] * fig_sc for i in range(fig_sc)
+            ]).reshape(-1, 1)
+
+            # get a batch to display
+            generated_images = generator.predict(
+                [noise, sampled_labels], verbose=0)
+            print("generated_images",generated_images.transpose(0,2,3,1).shape)
+        
+            #X, Y = np.meshgrid(range(32),range(32))
+            #Z = np.zeros((10,10))
+            plt.figure(num=None, figsize=(30, 30), dpi=60) #, facecolor='w', edgecolor='k')
+            for j in range(fig_sc):
+                for i in range(fig_sc):
+                    plt.subplot(fig_sc,fig_sc,1+i+fig_sc*j)
+                    plt.imshow(generated_images.transpose(0,2,3,1)[i+fig_sc*j][0:127][0:127],interpolation='bilinear') 
+                    plt.axis('off')
+                    plt.subplots_adjust(hspace = .001)
+                    #plt.tight_layout()
+            plt.pause(3)
+            plt.savefig('plot_epoch_{0:03d}_akb_generated.png'.format(epoch), dpi=60)
+            plt.close()
+            
+        else:
+            continue
+
+    pickle.dump({'train': train_history, 'test': test_history},
+                open('acgan-history.pkl', 'wb'))
+
